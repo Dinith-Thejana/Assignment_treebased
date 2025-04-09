a8aa617b9b2822de5f40e04bcb55201c8dd2be62
GimhaniEdiri <105563333@student.swin.edu.au>
2025-04-09 18:40:19 +1000

    Update search.py

--- search.py
+++ search.py
@@ -78,3 +78,22 @@ def parse_file(filename):
         sys.exit(1)
 
     return graph, origin, destinations
+
+def dfs(graph, start, goal):
+    stack = [(start, [start])]
+    visited = set()
+    
+    while stack:
+        node, path = stack.pop()
+        if node in visited:
+            continue
+        visited.add(node)
+
+        if node == goal:
+            return path
+        
+        for neighbor in graph.edges[node]:
+            if neighbor not in visited:
+                stack.append((neighbor, path + [neighbor]))
+    
+    return None
