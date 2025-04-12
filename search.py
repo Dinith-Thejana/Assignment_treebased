92e180261b7d690e4f793e07ae94afb9f88ed4e9
Harindu004 <105199657@student.swin.edu.au>
2025-04-12 10:22:13 +1000

    Update search.py

--- search.py
+++ search.py
@@ -76,3 +76,22 @@ def parse_file(filename):
         sys.exit(1)
 
     return graph, origin, destinations
+
+def bfs(graph, start, goal):
+    queue = [(start, [start])]
+    visited = set()
+    
+    while queue:
+        node, path = queue.pop(0)
+        if node in visited:
+            continue
+        visited.add(node)
+
+        if node == goal:
+            return path
+        
+        for neighbor in graph.edges[node]:
+            if neighbor not in visited:
+                queue.append((neighbor, path + [neighbor]))
+
+    return None
