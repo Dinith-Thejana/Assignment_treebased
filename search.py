7a3fed091f36ae97dbb6497229448d82102c2679
GimhaniEdiri <105563333@student.swin.edu.au>
2025-04-12 10:34:42 +1000

    Kim's Custom code

--- search.py
+++ search.py
@@ -114,3 +114,23 @@ def bfs(graph, start, goal):
                 queue.append((neighbor, path + [neighbor]))
 
     return None
+
+# Custom Search 1 - Iterative Deepening DFS
+def cus1(graph, start, goal):
+    def dls(node, goal, depth, path):
+        if depth == 0 and node == goal:
+            return path
+        if depth > 0:
+            for neighbor in graph.edges[node]:
+                if neighbor not in path:
+                    result = dls(neighbor, goal, depth - 1, path + [neighbor])
+                    if result:
+                        return result
+        return None
+
+    depth = 0
+    while True:
+        result = dls(start, goal, depth, [start])
+        if result:
+            return result
+        depth += 1
